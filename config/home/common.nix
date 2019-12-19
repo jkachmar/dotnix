2703d211e4027dd9ee0a8adc854095b41e410ae2
Joe Kachmar <joseph.kachmar@gmail.com>
2019-12-18 21:39:31 -0500

    Neovim is better anyway

--- config/home/common.nix
+++ config/home/common.nix
@@ -8,8 +8,10 @@
     ./fish.nix
     ./htop.nix
     ./mosh.nix
+    ./neovim.nix
     ./starship.nix
   ];
+
   programs = {
     # Let Home Manager install and manage itself.
     home-manager.enable = true;
---
+++ config/home/neovim.nix
@@ -0,0 +1,11 @@
+{ pkgs, ... }:
+
+{
+  programs = {
+    neovim = {
+      enable = true;
+      viAlias = true;
+      vimAlias = true;
+    };
+  };
+}
