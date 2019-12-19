c1d7d5c0a711b7a20402bf80a1af2bd91fee2882
Joe Kachmar <joseph.kachmar@gmail.com>
2019-12-18 21:28:49 -0500

    Ensure fish path ordering is correct

--- config/home/fish.nix
+++ config/home/fish.nix
@@ -7,6 +7,13 @@
       interactiveShellInit = ''
         fish_vi_key_bindings
       '';
+      shellInit = ''
+        for p in /run/current-system/sw/bin ~/.nix-profile/bin
+          if not contains $p $fish_user_paths
+            set -g fish_user_paths $p $fish_user_paths
+          end
+        end
+      '';
     };
   };
 }
