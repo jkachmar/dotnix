f0e42e694fc736c0e3f2956a6aa064d1b3e8396b
Joe Kachmar <me@jkachmar.com>
2020-04-27 00:35:48 -0400

    Adds ghcide-nix cache

--- config/common/nix/nixpkgs-caches.nix
+++ config/common/nix/nixpkgs-caches.nix
@@ -5,6 +5,10 @@ let
     "https://cache.nixos.org"
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=";
 
+  ghcide-nix = mkCache
+    "https://ghcide-nix.cachix.org"
+    "ghcide-nix.cachix.org-1:ibAY5FD+XWLzbLr8fxK6n8fL9zZe7jS+gYeyxyWYK5c=";
+
   iohk = mkCache
     "https://iohk.cachix.org"
     "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo=";
@@ -12,6 +16,7 @@ let
   caches = [
     default
     iohk
+    ghcide-nix
   ];
 in
 
