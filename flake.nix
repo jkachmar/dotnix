0b451e30406b40b231c671d90e909b6db7affd41
Joe Kachmar <me@jkachmar.com>
2020-09-10 01:26:30 -0400

    Uses nix flakes globally

--- flake.nix
+++ flake.nix
@@ -2,31 +2,33 @@
   description = "jkachmar's personal dotfiles and machine configurations";
 
   inputs = {
-    # Stable Darwin nixpkgs package set; pinned to the 20.03 release.
-    darwin.url = "github:nixos/nixpkgs-channels/nixpkgs-20.03-darwin";
     # Stable NixOS nixpkgs package set; pinned to the 20.03 release.
     stable.url = "github:nixos/nixpkgs-channels/nixos-20.03-small";
+    # Stable Darwin nixpkgs package set; pinned to the 20.03 release.
+    stableDarwin.url = "github:nixos/nixpkgs-channels/nixpkgs-20.03-darwin";
     # Unstable nixpkgs package set.
     #
     # More recent than the stable set, the results are likely to be cached.
-    unstable.url = "github:nixos/nixos-unstable-small";
+    unstable.url = "github:nixos/nixpkgs-channels/nixos-unstable-small";
     # Primary nixpkgs development repository
     #
     # Most recent package set, however the results are unlikely to be cached.
     trunk.url = "github:nixos/nixpkgs";
     # TODO: Switch to nix-darwin's main branch when flakes have been merged.
     nix-darwin.url = "github:LnL7/nix-darwin/flakes";
-    home-manager.url = "github:nix-community/home-manager";
-    emacs-overlay.url = "github:nix-community/emacs-overlay";
+    home.url = "github:nix-community/home-manager";
+    emacs.url = "github:nix-community/emacs-overlay";
   };
 
-  outputs = inputs: {
+  outputs = inputs@{ self, stable, stableDarwin, ... }: {
+    overlays = {};
+
     nixosConfigurations = {
-      star-platinum = {}
+      star-platinum = {};
     };
 
     darwinConfigurations = {
-      crazy-diamond = {}
+      crazy-diamond = {};
     };
   };
 }
