82dbaf3f69f90bea7e0a7b6d4132d4dcfcc91942
Joe Kachmar <joseph.kachmar@well.co>
2020-10-28 12:01:57 -0400

    Updates highway-star

--- config/machines/highway-star/default.nix
+++ config/machines/highway-star/default.nix
@@ -1,28 +1,47 @@
 { ... }:
 let
-  sources = import ../../nix/sources.nix;
+  sources = import ../../../nix/sources.nix;
+
+  # NOTE: Replicated (approximately) from flake config for the time being...
+  overlays = {
+    # Inject 'unstable' and 'trunk' into the overridden package set, so that
+    # the following overlays may access them (along with any system configs
+    # that wish to do so).
+    pkg-sets = final: prev: {
+      unstable = import sources.unstable { };
+      trunk = import sources.trunk { };
+    };
+
+    overridden_pkgs = import ../../../overlays/overridden_pkgs.nix;
+    pinned_pkgs = import ../../../overlays/pinned_pkgs.nix;
+    custom_pkgs = import ../../../overlays/custom_pkgs.nix;
+  };
 in
 {
   imports = [
     "${sources.home-manager}/nix-darwin"
     ./hardware.nix
-    ../../config/nix
-    ../../modules/nix
-    ../../modules/system/darwin.nix
-    ../../modules/desktop
-    ../../modules/development
+    ../../profiles/macos
   ];
 
-  # Inject sources pinned with Niv to be used elsewhere.
-  _module.args.sources = sources;
+  # NOTE: Replicated (approximately) from flake config for the time being...
+  _module.args.inputs = {
+    inherit (sources) darwin-stable nix-darwin unstable trunk;
+    self.overlays = overlays;
+  };
 
-  # primary-user.email = ""; # TODO: store this in secrets
+  primary-user.email = "joseph.kachmar@well.com";
   primary-user.fullname = "Joe Kachmar";
-  # primary-user.username = ""; # TODO: store this in secrets
+  primary-user.username = "jkachmar";
   networking.hostName = "highway-star";
 
+  #############################################################################
+  # Used for backwards compatibility, please read the changelog before changing
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+
   ###############################################################################
-  # Machine-specific, user-level configuration.
+  # Machine-specific configuration.
   primary-user.home-manager = { pkgs, ... }: {
     home.packages = with pkgs; [
       awscli
@@ -30,23 +49,10 @@ in
     ];
   };
 
-
   # This machine _must_ be run in single-user mode, so we cannot take advantage
   # of the Nix daemon.
-  services.nix-daemon = {
+  services.nix-daemon.enable = lib.mkForce {
     enable = false;
     enableSocketListener = false;
   };
-
-  # NOTE: If `environment.darwinConfig` is _not_ set, then nix-darwin defaults
-  # to some location in the home directory
-  #
-  # If it's set _in addition to_ `darwin-config` on the NIX_PATH, then
-  # duplicate entries will be present
-  environment.darwinConfig = ./.;
-
-  #############################################################################
-  # Used for backwards compatibility, please read the changelog before changing
-  # $ darwin-rebuild changelog
-  system.stateVersion = 4;
 }
