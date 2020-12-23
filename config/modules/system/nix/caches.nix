f73ad126824a706f3f535eccb3a252e3bc2054eb
Joe Kachmar <me@jkachmar.com>
2020-12-23 15:41:45 -0500

    Evicts IOHK cache

--- config/modules/system/nix/caches.nix
+++ config/modules/system/nix/caches.nix
@@ -12,17 +12,12 @@ let
     "https://ghcide-nix.cachix.org"
     "ghcide-nix.cachix.org-1:ibAY5FD+XWLzbLr8fxK6n8fL9zZe7jS+gYeyxyWYK5c=";
 
-  iohk = mkCache
-    "https://hydra.iohk.io"
-    "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=";
-
   nix-community = mkCache
     "https://nix-community.cachix.org"
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
 
   caches = [
     default
-    iohk
     ghcide-nix
     nix-community
   ];
