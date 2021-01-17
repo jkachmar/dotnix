449b119510ecdba81c571bef2658cbadcc9edbf0
Joe Kachmar <me@jkachmar.com>
2021-01-17 18:25:13 -0500

    Adds more fonts

--- config/modules/system/fonts/default.nix
+++ config/modules/system/fonts/default.nix
@@ -8,10 +8,11 @@
     enableFontDir = true;
     fonts = with pkgs; [
       emacs-all-the-icons-fonts
-      hack-font
-      fira-code
       font-awesome_5
-      noto-fonts
+      hack-font
+      ibm-plex
+      jetbrains-mono
+      overpass
     ];
   };
 }
