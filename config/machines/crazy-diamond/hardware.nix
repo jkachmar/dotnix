a97dca087b3d39230e7a8b4839d69d9327a9f894
jkachmar <git@jkachmar.com>
2021-03-25 20:00:21 -0400

    Fresh start to pull in updated config

--- config/machines/crazy-diamond/hardware.nix
+++
@@ -1,9 +0,0 @@
-{ ... }:
-
-{
-  # This should generally be set to machine's number of logical cores.
-  nix = {
-    buildCores = 4;
-    maxJobs = 4;
-  };
-}
--- overlays/pinned_pkgs.nix
+++
@@ -1,3 +0,0 @@
-# Packages that are pinned with newer (or different) versions than are present
-# in the nixpkgs package tree.
-_final: _prev: { }
