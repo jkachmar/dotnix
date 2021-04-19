a5ce50221c8d08aea6debdc62330ea3c989cc525
jkachmar <me@jkachmar.com>
2021-04-19 01:10:22 -0400

    Adds nerd fonts

--- config/modules/ui/fonts.nix
+++ config/modules/ui/fonts.nix
@@ -7,12 +7,30 @@
   fonts = {
     enableFontDir = true;
     fonts = with pkgs; [
+      # Icon fonts.
       emacs-all-the-icons-fonts
       font-awesome_5
+
+      # "Actual" fonts.
       hack-font
       ibm-plex
       jetbrains-mono
       overpass
+
+      # Various fonts with nerd symbols.
+      #
+      # NOTE: Try to keep this in-line with fonts from the above list of
+      # "actual" fonts.
+      #
+      # cf. https://github.com/NixOS/nixpkgs/blob/master/pkgs/data/fonts/nerdfonts/shas.nix
+      (nerdfonts.override { 
+        fonts = [
+          "Hack"
+          "IBMPlexMono"
+          "JetBrainsMono"
+          "Overpass"
+        ];
+      })
     ];
   };
 }
