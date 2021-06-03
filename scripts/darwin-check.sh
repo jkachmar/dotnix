81a836d5b1320fc50b52343d9d2ead12ba579f87
jkachmar <me@jkachmar.com>
2021-06-03 10:39:15 -0400

    Adds script to check darwin rebuild

---
+++ scripts/darwin-check.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+
+set -euo pipefail
+
+machine=$1
+
+CONFIG_PATH="${HOME}/.config/dotfiles"
+
+nix build \
+  --experimental-features 'flakes nix-command' \
+  --show-trace \
+  --no-link "${CONFIG_PATH}/#darwinConfigurations.${machine}.system"
+
+result=$(nix path-info "${HOME}/.config/dotfiles/#darwinConfigurations.${machine}.system")
+
+"${result}"/sw/bin/darwin-rebuild --flake .#"${machine}" check
