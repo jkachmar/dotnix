a8466aeb18c7c9a316937bd7a0130c5c37e4b6f1
jkachmar <me@jkachmar.com>
2021-06-03 10:39:03 -0400

    Updates to the 21.05 release

--- flake.nix
+++ flake.nix
@@ -10,11 +10,11 @@
     #
     # `darwin` is used to indicate the most up-to-date stable packages tested
     # against macOS.
-    macosPkgs.url = "github:nixos/nixpkgs/nixpkgs-20.09-darwin";
+    macosPkgs.url = "github:nixos/nixpkgs/nixpkgs-21.05-darwin";
     # Stable NixOS package set; pinned to the latest 20.09 release.
     #
     # `small` is used to indicate the most up-to-date stable packages.
-    nixosPkgs.url = "github:nixos/nixpkgs/nixos-20.09-small";
+    nixosPkgs.url = "github:nixos/nixpkgs/nixos-21.05-small";
     # Unstable (rolling-release) NixOS package set.
     unstable.url = "github:nixos/nixpkgs";
 
@@ -29,7 +29,7 @@
     #
     # cf. https://github.com/LnL7/nix-darwin/issues/255
     darwin = {
-      inputs.nixpkgs.follows = "unstable";
+      inputs.nixpkgs.follows = "macosPkgs";
       url = "github:lnl7/nix-darwin";
     };
 
@@ -40,20 +40,20 @@
     #
     # cf. https://github.com/LnL7/nix-darwin/issues/255
     macosHome = {
-      inputs.nixpkgs.follows = "unstable";
-      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "macosPkgs";
+      url = "github:nix-community/home-manager/release-21.05";
     };
     # Declarative user configuration for NixOS systems.
     nixosHome = {
       inputs.nixpkgs.follows = "nixosPkgs";
-      url = "github:nix-community/home-manager/release-20.09";
+      url = "github:nix-community/home-manager/release-21.05";
     };
 
     # Declarative, persistent state management for ephemeral systems.
     impermanence.url = "github:nix-community/impermanence";
   };
 
-  outputs = inputs@{ self, darwin, nixosPkgs, unstable, ... }:
+  outputs = inputs@{ self, darwin, macosPkgs, nixosPkgs, unstable, ... }:
     let
       # Utility function to construct a package set based on the given system
       # along with the shared `nixpkgs` configuration defined in this repo.
@@ -76,14 +76,10 @@
           (./. + "/config/machines/${hostname}")
         ];
         specialArgs = {
-          # XXX: Tracking unstable to work around compatibility issues with
-          # macOS 11.x (Big Sur).
           inputs = inputs // {
-            nixpkgs = unstable;
+            nixpkgs = macosPkgs;
           };
-          # XXX: Tracking unstable to work around compatibility issues with
-          # macOS 11.x (Big Sur).
-          pkgs = mkPkgsFor system unstable;
+          pkgs = mkPkgsFor system macosPkgs;
           unstable = mkPkgsFor system unstable;
         };
       };
