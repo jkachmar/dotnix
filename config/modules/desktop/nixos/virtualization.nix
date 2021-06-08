4f964f4744eae6da44bd1ee9e23935ddd23d9f46
jkachmar <git@jkachmar.com>
2021-06-07 20:53:00 -0400

    Removes virtualbox (compilation time is too long)

--- config/modules/desktop/nixos/virtualization.nix
+++ config/modules/desktop/nixos/virtualization.nix
@@ -9,12 +9,15 @@
     enableNvidia = true;
   };
 
+  # TODO: Abstract this out to the `primary-user` config module.
+  users.users.jkachmar.extraGroups = [ "docker" ];
+
   # NOTE: Technically not machine-agnostic, since it's for NVIDIA support
   # within VirtualBox.
   hardware.opengl.driSupport32Bit = true;
-  virtualisation.virtualbox.host = {
-    enable = true;
-    enableExtensionPack = true;
-  };
-  users.extraGroups.vboxusers.members = [ config.primary-user.username ];
+  # virtualisation.virtualbox.host = {
+  #   enable = true;
+  #   enableExtensionPack = true;
+  # };
+  # users.extraGroups.vboxusers.members = [ config.primary-user.name ];
 }
