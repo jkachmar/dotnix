859964583554c8d00152f1edfacd51d25632ba5b
Joe Kachmar <me@jkachmar.com>
2021-06-17 13:08:08 -0400

    Adds plex config

---
+++ config/modules/services/media/hardware-acceleration.nix
@@ -0,0 +1,33 @@
+# 
+{ pkgs, ... }:
+{
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      # Hardware transcoding.
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      libvdpau-va-gl
+      vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but can work better for some applications)
+      vaapiVdpau
+      # HDR tone mapping.
+      beignet
+      intel-compute-runtime
+      intel-ocl
+      ocl-icd
+    ];
+    # 32-bit support.
+    driSupport32Bit = true;
+    extraPackages32 = with pkgs; [
+      # Hardware transcoding.
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      libvdpau-va-gl
+      vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but can work better for some applications)
+      vaapiVdpau
+      # HDR tone mapping.
+      beignet
+      intel-compute-runtime
+      intel-ocl
+      ocl-icd
+    ];
+  };
+}
---
+++ config/modules/services/media/plex.nix
@@ -0,0 +1,48 @@
+# 
+{ lib, pkgs, ... }:
+
+let
+  inherit (lib) fakeSha256;
+in
+
+{
+  # Create a "plex" user for the service, so a user ID can be manually set.
+  #
+  # XXX: The NixOS Plex module checks if `services.plex.user == "plex"`, so the
+  # custom plex user defined here needs to have a different name.
+  #
+  # TODO: Manually synchronize the UID and GID with whatever Synology is
+  # doing...
+  users.users.plexuser = {
+    uid = 1001;
+    extraGroups = [ "media" ];
+    isNormalUser = false;
+    isSystemUser = true;
+  };
+  users.groups.plexgroup.gid = 1002;
+
+  services.plex = {
+    enable = true;
+    dataDir = "/state/plex";
+    user = "plexuser"; # XXX: Matches custom Plex user defined below.
+    group = "plexgroup"; # XXX: Matches custom Plex group defined below.
+    openFirewall = true;
+    # XXX: Keep up-to-date with:
+    #
+    # https://plex.tv/api/downloads/5.json?channel=plexpass
+    #
+    # TODO: Adapt a more automatic solution with a script:
+    #
+    # https://github.com/tadfisher/flake/blob/a947b381fc7fb48aa22c29ac4e7149e9f46a9a85/pkgs/plex-plexpass/update.sh
+    package = pkgs.plex.override {
+      plexRaw = pkgs.plexRaw.overrideAttrs (attrs: rec {
+        pname = attrs.pname + "-plexpass";
+        version = "1.23.2.4656-85f0adf5b";
+        src = builtins.fetchurl {
+          url = "https://downloads.plex.tv/plex-media-server-new/1.23.2.4656-85f0adf5b/redhat/plexmediaserver-1.23.2.4656-85f0adf5b.x86_64.rpm";
+          sha256 = "1xkasddnkwwi1a1hn967fhqnazqs61riylif349mhvkd4v2z5rrc";
+        };
+      });
+    };
+  };
+}
