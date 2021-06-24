9a561c1337c652b8d78fc0e463534f3b21024b70
jkachmar <git@jkachmar.com>
2021-06-24 12:28:57 -0400

    Fixes openssh module typo

--- config/security/openssh.nix
+++ config/security/openssh.nix
@@ -5,16 +5,17 @@
     enable = true;
     passwordAuthentication = false;
     permitRootLogin = "no";
+
+    hostKeys =  [
+      {
+        path = "/secrets/openssh/host/ssh_host_ed25519_key";
+        type = "ed25519";
+      }
+      {
+        path = "/secrets/openssh/host/ssh_host_rsa_key";
+        type = "rsa";
+        bits = 4096;
+      }
+    ];
   };
-  hostKeys =  [
-    {
-      path = "/secrets/openssh/host/ssh_host_ed25519_key";
-      type = "ed25519";
-    }
-    {
-      path = "/secrets/openssh/host/ssh_host_rsa_key";
-      type = "rsa";
-      bits = 4096;
-    }
-  ];
 }
