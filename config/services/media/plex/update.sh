125e13d9c3b28b8327c31f5be9bd8c8f79037fbb
Joe Kachmar <me@jkachmar.com>
2021-07-10 18:30:22 -0400

    Reverts plex to non-beta version

--- config/services/media/plex/update.sh
+++ config/services/media/plex/update.sh
@@ -13,7 +13,8 @@ declare -A platforms=(
 )
 
 token=$(cat "/secrets/plex/token")
-manifest=$(curl -s "https://plex.tv/api/downloads/5.json?channel=plexpass" -H "X-Plex-Token: ${token}")
+# manifest=$(curl -s "https://plex.tv/api/downloads/5.json?channel=plexpass" -H "X-Plex-Token: ${token}")
+manifest=$(curl -s "https://plex.tv/api/downloads/5.json" -H "X-Plex-Token: ${token}")
 version=$(echo "$manifest" | jq -r '.computer.Linux.version | split("-") | .[0]')
 
 tmp="$path/sources.tmp.json"
