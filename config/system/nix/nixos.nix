0d3236398408930d0729c3c0e4f1672e4b96e229
jkachmar <git@jkachmar.com>
2021-08-27 16:49:03 -0400

    Enables store optimization by default

--- config/system/nix/nixos.nix
+++ config/system/nix/nixos.nix
@@ -1,11 +1,14 @@
 #######################################
 # NixOS-specific `nix` configuration. #
 #######################################
-{ ... }:
+{ lib, ... }:
 {
   # OS-agnostic `nix` configuration.
   imports = [ ./common.nix ];
   nix = {
+    # NOTE: Pretty sure this should be the default anyway...
+    autoOptimiseStore = lib.mkDefault true;
+
     useSandbox = true;
     # TODO: Figure out what automatic garbage collection settings work best.
     # gc = {
