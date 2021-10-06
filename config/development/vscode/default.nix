2d98889c2b8e513f08a8aa78dec45f0017b761e9
jkachmar <git@jkachmar.com>
2021-10-05 22:39:19 -0400

    Updates VS Code

--- config/development/vscode/default.nix
+++ config/development/vscode/default.nix
@@ -1,16 +1,34 @@
 ###############################################
 # OS-agnostic Visual Studio Code configuration. #
 #################################################
-{ pkgs, ... }:
+{ pkgs, unstable, ... }:
 
 {
   primary-user.home-manager = { config, ... }:
     let
       inherit (config.lib.file) mkOutOfStoreSymlink;
+
+      # Copied from `home-manager` source.
+      configDir = "Code";
+      userDir = if pkgs.stdenv.hostPlatform.isDarwin then
+        "Library/Application Support/${configDir}/User"
+      else
+        "${config.xdg.configHome}/${configDir}/User";
+      configFilePath = "${userDir}/settings.json";
     in
     {
-      programs.vscode.enable = true;
-      xdg.configFile."Code/vscode/settings.json".source = 
+      programs.vscode = {
+        enable = true;
+        package = unstable.vscode;
+        # Use up-to-date extensions.
+        extensions = (with unstable.vscode-extensions; [
+          vadimcn.vscode-lldb
+          # TODO: Check out neovim-vscode
+          vscodevim.vim
+        ]);
+      };
+
+      home.file."${configFilePath}".source = 
         mkOutOfStoreSymlink "${config.xdg.configHome}/dotfiles/config/development/vscode/settings.json";
     };
 }
