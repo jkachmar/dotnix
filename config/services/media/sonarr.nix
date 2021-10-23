433b65d46256e0a98f9c94f9a3613ff4ef9fcab9
jkachmar <git@jkachmar.com>
2021-10-22 20:47:35 -0400

    Adds sonarr to multi-user startup target dep graph

--- config/services/media/sonarr.nix
+++ config/services/media/sonarr.nix
@@ -14,6 +14,7 @@ in
   # Ensure that sonarr waits for the downloads and media directories to be 
   # available.
   systemd.services.sonarr = {
+    wantedBy = [ "multi-user.target" ];
     after = [
       "network.target"
       "mnt-moodyblues-downloads.mount"
