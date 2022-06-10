b3fa5ed0a781726a3541008ab23e9814ba43fb75
jkachmar <git@jkachmar.com>
2022-06-09 20:18:13 -0400

    Fish direnv integration is non-optional

--- config/development/direnv.nix
+++ config/development/direnv.nix
@@ -10,8 +10,6 @@
     # Conditionally enable shell integrations.
     enableBashIntegration =
       config.primary-user.home-manager.programs.bash.enable;
-    enableFishIntegration =
-      config.primary-user.home-manager.programs.fish.enable;
     enableZshIntegration =
       config.primary-user.home-manager.programs.zsh.enable;
   };
