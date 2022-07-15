f7f93eb772d182ddd2d0b8c38b055664600baf91
jkachmar <git@jkachmar.com>
2022-07-14 23:40:30 -0400

    Adds Synology reverse proxy config

--- config/services/nginx.nix
+++ config/services/nginx.nix
@@ -1,6 +1,7 @@
 { config, ... }:
 
 let
+  inherit (config.networking) domain;
   email = config.primary-user.email;
 in
 {
@@ -34,12 +35,23 @@ in
         extraDomainNames = [
           "*.thempire.dev"
           "*.enigma.thempire.dev"
+          "*.moody-blues.thempire.dev"
         ];
 
         # Use Cloudflare's DNS resolver rather than the system-provided one to
         # ensure that everything propagates as quickly as possible.
         extraLegoFlags = [ "--dns.resolvers=1.1.1.1:53" ];
       };
+    };
+  };
+
+  services.nginx.virtualHosts = {
+    # NOTE: Ensure this server is trusted by adding its IP to the NAS via
+    # 'Control Panel' -> 'Security' -> 'Trusted Proxies' in the DSM webapp.
+    "moody-blues.${domain}" = {
+      forceSSL = true;
+      useACMEHost = domain;
+      locations."/".proxyPass = "https://10.0.1.250:5001";
     };
   };
 }
