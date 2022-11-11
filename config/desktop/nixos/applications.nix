6eea302c6eda2bbbf7dcab860c5db20b15797323
jkachmar <git@jkachmar.com>
2022-11-11 08:58:28 -0500

    [nixos-desktop] use vanilla signal-desktop

--- config/desktop/nixos/applications.nix
+++ config/desktop/nixos/applications.nix
@@ -2,20 +2,6 @@
 # Misc. NixOS desktop applications. #
 #####################################
 { pkgs, unstable, ... }:
-let
-  # # TODO: Move this out to an overlay (i.e. figure out how overlays are going
-  # # to work with a Flakes-based configuration).
-  # updated-signal-desktop = pkgs.signal-desktop.overrideAttrs (_oldAttrs: rec {
-  #   version = "1.40.0";
-  #   src = pkgs.fetchurl {
-  #     url = "https://updates.signal.org/desktop/apt/pool/main/s/signal-desktop/signal-desktop_${version}_amd64.deb";
-  #     sha256 = "1xd38a9mi23c4r873k37rzip68hfk3a4bk9j4j24v2kb3yvixrpp";
-  #   };
-  # });
-  updated-signal-desktop = pkgs.signal-desktop.override {
-    spellcheckerLanguage = "en_US";
-  };
-in
 {
   # TODO: lol this is gonna probably need some customization.
   # TODO: Figure out overlays and see how difficult it is to pull in an
@@ -32,14 +18,12 @@ in
       slack
       xclip # Clipboard selection a la `cat foo | xclip -selection clipboard`
       zoom-us
-
-      # Custom/overridden packages.
-      updated-signal-desktop
     ]) ++ (with unstable; [
       # NOTE: Discord sometimes refuses to open without the latest version.
       #
       # cf. https://nixos.wiki/wiki/Discord#Discord_wants_latest_version
       discord
+      signal-desktop
     ]);
 
     # XXX: The `mimeApps` stuff is a little weird, see the following issues for
