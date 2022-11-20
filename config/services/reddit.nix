b8982a18b971d23001e8060836973d5623aad2ba
jkachmar <git@jkachmar.com>
2022-11-20 16:19:54 -0500

    [enigma] adds libreddit

---
+++ config/services/reddit.nix
@@ -0,0 +1,27 @@
+{ config, pkgs, ... }:
+
+let
+  inherit (config.networking) domain hostName;
+  fqdn = "${hostName}.${domain}";
+  listen = {
+    address = "0.0.0.0";
+    port = 5050;
+  };
+in
+{
+  services.libreddit = {
+    enable = true;
+    inherit (listen) address port;
+    # redirect = true;
+    openFirewall = false;
+  };
+
+  services.nginx.virtualHosts."reddit.${fqdn}" = {
+    forceSSL = true;
+    useACMEHost = domain;
+    locations."/" = {
+      proxyPass = "http://${listen.address}:${builtins.toString listen.port}";
+      proxyWebsockets = true;
+    };
+  };
+}
