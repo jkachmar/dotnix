3e1404bd89c0695682f8188e993ab15e3c096d72
jkachmar <git@jkachmar.com>
2023-05-02 20:11:54 -0400

    yeet old systems

--- flake.nix
+++ flake.nix
@@ -5,12 +5,6 @@
     ################
     # PACKAGE SETS #
     ################
-
-    # Stable macOS package set; pinned to the latest 22.05 release.
-    #
-    # `darwin` is used to indicate the most up-to-date stable packages tested
-    # against macOS.
-    macosPkgs.url = "github:nixos/nixpkgs/nixpkgs-22.05-darwin";
     # Stable NixOS package set; pinned to the latest 22.05 release.
     nixosPkgs.url = "github:nixos/nixpkgs/nixos-22.05";
     # Unstable (rolling-release) NixOS package set.
@@ -19,18 +13,6 @@
     #############
     # UTILITIES #
     #############
-
-    # Declarative, NixOS-style configuration for macOS.
-    darwin = {
-      inputs.nixpkgs.follows = "macosPkgs";
-      url = "github:lnl7/nix-darwin";
-    };
-
-    # Declarative user configuration for macOS systems.
-    macosHome = {
-      inputs.nixpkgs.follows = "macosPkgs";
-      url = "github:nix-community/home-manager/release-21.11";
-    };
     # Declarative user configuration for NixOS systems.
     nixosHome = {
       inputs.nixpkgs.follows = "nixosPkgs";
@@ -38,7 +20,7 @@
     };
   };
 
-  outputs = inputs@{ self, darwin, macosPkgs, nixosPkgs, unstable, ... }:
+  outputs = inputs@{ self, nixosPkgs, unstable, ... }:
     let
       # Utility function to construct a package set based on the given system
       # along with the shared `nixpkgs` configuration defined in this repo.
@@ -48,32 +30,11 @@
           config = import ./config/system/nixpkgs/config.nix;
         };
 
-      # Utility function to construct a macOS configuration for arbitrary
-      # systems.
-      #
-      # TODO: Push more of this functionality down down into the
-      # `./config/machines` modules to avoid cluttering up `flake.nix` any
-      # more than is necessary.
-      mkMacOSConfiguration = hostname: system: darwin.lib.darwinSystem {
-        modules = [
-          inputs.macosHome.darwinModule
-          # XXX: Nix needs to believe we have an absolute path here.
-          (./. + "/hosts/${hostname}")
-        ];
-        specialArgs = {
-          inputs = inputs // {
-            nixpkgs = macosPkgs;
-          };
-          pkgs = mkPkgsFor system macosPkgs;
-          unstable = mkPkgsFor system unstable;
-        };
-      };
-
       # Utility function to construct a NixOS configuration for arbitrary
       # systems.
       #
       # TODO: Push more of this functionality down down into the
-      # `./config/machines` modules to avoid # cluttering up `flake.nix` any
+      # `./config/machines` modules to avoid cluttering up `flake.nix` any
       # more than is necessary.
       mkNixOSConfiguration = hostname: system: nixosPkgs.lib.nixosSystem {
         inherit system;
@@ -91,14 +52,8 @@
       };
     in
     {
-      darwinConfigurations = {
-        crazy-diamond = mkMacOSConfiguration "crazy-diamond" "x86_64-darwin";
-        manhattan-transfer = mkMacOSConfiguration "manhattan-transfer" "aarch64-darwin";
-      };
-
       nixosConfigurations = {
         enigma = mkNixOSConfiguration "enigma" "x86_64-linux";
-        kraftwerk = mkNixOSConfiguration "kraftwerk" "x86_64-linux";
         star-platinum = mkNixOSConfiguration "star-platinum" "x86_64-linux";
       };
     };
