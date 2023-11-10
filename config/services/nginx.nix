827bb7558d30d10b484f8b7620c7273d7cc865fc
jkachmar <git@jkachmar.com>
2023-11-09 23:03:45 -0500

    move synology rev proxy to tatl

--- config/services/nginx.nix
+++ config/services/nginx.nix
@@ -42,39 +42,6 @@ in
         # ensure that everything propagates as quickly as possible.
         extraLegoFlags = [ "--dns.resolvers=1.1.1.1:53" ];
       };
-      "moody-blues.${domain}" = {
-        inherit email;
-        credentialsFile = "/secrets/cloudflare/acme.env";
-        dnsProvider = "cloudflare";
-        extraDomainNames = [ "*.moody-blues.${domain}" ];
-
-        # Use Cloudflare's DNS resolver rather than the system-provided one to
-        # ensure that everything propagates as quickly as possible.
-        extraLegoFlags = [ "--dns.resolvers=1.1.1.1:53" ];
-      };
-    };
-  };
-
-  services.nginx.virtualHosts = {
-    # NOTE: Ensure this server is trusted by adding its IP to the NAS via
-    # 'Control Panel' -> 'Security' -> 'Trusted Proxies' in the DSM webapp.
-    "moody-blues.${domain}" = {
-      forceSSL = true;
-      useACMEHost = "moody-blues.${domain}";
-      locations."/" = {
-        proxyPass = "https://192.168.1.155:5001";
-        extraConfig = ''
-          # Causes issues w/ uploading very large files via Synology web UI.
-          client_max_body_size 0;
-          proxy_request_buffering off;
-        '';
-      };
-    };
-
-    "webdav.moody-blues.${domain}" = {
-      forceSSL = true;
-      useACMEHost = "moody-blues.${domain}";
-      locations."/".proxyPass = "https://192.168.1.155:5006";
     };
   };
 }
